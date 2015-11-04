e1ffb51629f066a085fc50554d414691b941d59d
sanjeo <sanjeo@163.com>
2015-11-04 15:06:02 +0800

    Update the implemenation of OpenStackCredentials related classes
    Signed-off-by: sanjeo <sanjeo@163.com>

--- src/main/java/com/cloudera/director/openstack/OpenStackCredentials.java
+++ src/main/java/com/cloudera/director/openstack/OpenStackCredentials.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright (c) 2015 Intel Corporation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.cloudera.director.openstack;
 
 public class OpenStackCredentials {
@@ -6,25 +21,43 @@ public class OpenStackCredentials {
 	private final String identity;
 	private final String credential;
 
-	public OpenStackCredentials(String endpoint, String tenant, String user, String credential){
+	public OpenStackCredentials(String endpoint, String tenant, String user, String credential) {
+		String nullArg = "";
+		if (endpoint == null) {
+			nullArg += "The Keystone Endpoint ";
+		}
+		
+		if (tenant == null || user == null) {
+			nullArg += ", Identity(tenant and user) ";
+		}
+		
+		if (credential == null) {
+			nullArg += "and the credential ";
+		}
+		
 		this.keystone_endpoint = endpoint;
 		this.identity = tenant + ":" + user;
 		this.credential = credential;
+		
+		if (!nullArg.isEmpty()) {
+			throw new IllegalArgumentException(nullArg + "are(is) empty."  );
+		}
+		
 	}
 	
-	public String getEndpoint(){
+	public String getEndpoint() {
 		return keystone_endpoint;
 	}
 	
-	public String getIdentity(){
+	public String getIdentity() {
 		return identity;
 	}
 	
-	public String getCredential(){
+	public String getCredential() {
 		return credential;
 	}
 	
-	public boolean equals(OpenStackCredentials cre){
+	public boolean equals(OpenStackCredentials cre) {
 		return keystone_endpoint.equals(cre.getEndpoint()) && 
 			   identity.equals(cre.getIdentity()) &&
 			   credential.equals(cre.getCredential());
